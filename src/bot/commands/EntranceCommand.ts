10cbd88494e7d40e1ff56e8811dcfcb9f8596e6b
Zach Pierce <zpierce@reviewtrackers.com>
2020-06-30 18:31:14 -0500

    merge conflict

--- src/bot/commands/EntranceCommand.ts
+++ src/bot/commands/EntranceCommand.ts
@@ -1,4 +1,4 @@
-import { Message, Permissions } from 'discord.js';
+import { Message } from 'discord.js';
 
 import * as entrances from '@util/db/Entrances';
 import { getSounds } from '@util/SoundUtil';
@@ -6,35 +6,17 @@ import Command from './base/Command';
 
 export default class EntranceCommand implements Command {
   public readonly TRIGGERS = ['entrance'];
-  public readonly NUMBER_OF_PARAMETERS = 2;
-  public readonly USAGE = 'Usage: !entrance <sound> ?<userId>';
 
   public run(message: Message, params: string[]) {
-    let entranceSound;
-    let userId;
-    if (params.length > this.NUMBER_OF_PARAMETERS) {
-      message.channel.send(this.USAGE);
-      return;
-    }
-
-    if (params.length === 1) {
-      [entranceSound] = params;
-      userId = message.author.id;
-    } else {
-      if (!message.member) return;
-      if (!message.member.hasPermission(Permissions.FLAGS.ADMINISTRATOR!)) return;
-
-      [entranceSound, userId] = params;
-    }
-
+    const [entranceSound] = params;
     if (!entranceSound) {
-      entrances.remove(userId);
+      entrances.remove(message.author.id);
       return;
     }
 
     const sounds = getSounds();
     if (!sounds.includes(entranceSound)) return;
 
-    entrances.add(userId, entranceSound);
+    entrances.add(message.author.id, entranceSound);
   }
 }
